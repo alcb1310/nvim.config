b1e2f180a0645a7efb18aa3c430c4dd9cfe90ed1
Andrés Court <andres@andrescourt.com>
2023-09-07 17:55:56 -0500

    Added format on save

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,6 +1,18 @@
 local lsp = require("lsp-zero")
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 lsp.preset("recommended")
+local lsp_format_on_save = function(bufnr)
+  vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    group = augroup,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format()
+    end,
+  })
+end
+
 
 lsp.ensure_installed({
      'tsserver',
@@ -14,7 +26,7 @@ lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -40,7 +52,7 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-     local opts = {buffer = bufnr, remap = false}
+     local opts = { buffer = bufnr, remap = false }
 
      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -52,9 +64,11 @@ lsp.on_attach(function(client, bufnr)
      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
      vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+     lsp_format_on_save(bufnr)
 end)
 
-lsp.setup_servers({ 'tsserver', 'eslint', 'gopls'})
+lsp.setup_servers({ 'tsserver', 'eslint', 'gopls' })
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
